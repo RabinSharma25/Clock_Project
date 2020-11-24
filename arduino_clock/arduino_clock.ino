20410f1e1118f2d28f62c880df1fd055db0c3b4f
Robin Sharma <cruzosharmas22545@gmail.com>
2020-11-24 21:41:31 +0530

    Added input methods from the computer to setup the time in the module

--- arduino_clock/arduino_clock.ino
+++ arduino_clock/arduino_clock.ino
@@ -1,90 +1,393 @@
-#include<Wire.h>
-#define deviceAddress 0b1101000 //0x68
-#include<LiquidCrystal_I2C.h>
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+#include<EEPROM.h>
+
+int integer;     // inital integer which we take from the user
+int divisor = 2; // global divisor
+
+int last_digits;  // for seconds
+int first_digits; // for seconds
+//int *nums;
+int bits[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+int flag2 = 0;
+
+String bitval;
+String sec, minss, hr, day, date, month, year;
+int flag;
+
+int secs,mins,hrs,days,dates,months,years; // converting to int (variables to store data);
+
+int bitvals;
 
 void setup()
 {
   Serial.begin(9600);
-  Wire.begin();   //I2C Bus is formed/eanbaled
-  lcd.begin();
-  lcd.backlight();
-  lcd.setCursor(0, 0); //DP0 of Topline
+
+String entered_day;
+
+
+    String names;
+    //int v;
+    Serial.println("Enter the initial time in seconds  ");
+    while (Serial.available()==0){
+    
+    }
+    integer = Serial.parseInt();
+    digit_splitter(integer);
+    binary_converter(first_digits, last_digits);
+    bits_conc();
+    initial_bits();
+    //Serial.print(/n);
+    flag = 1;
+    flag2 = 1;
+
+    Serial.println("Enter the initial time in  minutes ") ;
+    while (Serial.available()==0){
+   
+    }
+     integer = Serial.parseInt();
+    digit_splitter(integer);
+    binary_converter(first_digits, last_digits);
+    bits_conc();
+    initial_bits();
+    //Serial.println(/n);
+    flag = 2;
+    flag2 = 2;
+
+    Serial.println("Enter the initial time in hours in whichever format you want that is 12 or 24 " );
+    while(Serial.available() ==0){
+    }
+    integer = Serial.parseInt();
+    digit_splitter(integer);
+    binary_converter(first_digits, last_digits);
+    bits_conc();
+    initial_bits();
+    //Serial.println(/n);
+    flag = 0;
+    flag2 = 3;
+
+    Serial.println( "Enter the day which you want to set initially " );
+    while(Serial.available() ==0){
+    }
+     entered_day =Serial.readString();
+    day_set(entered_day);
+    names = bitval;
+    Serial.println(names);
+    //cout << endl;
+    flag2 = 4;
+
+    Serial.println("Enter the initial date ") ;
+      while (Serial.available()==0){
+    
+    }
+    integer = Serial.parseInt();
+    digit_splitter(integer);
+    binary_converter(first_digits, last_digits);
+    bits_conc();
+    initial_bits();
+    //cout << endl;
+    flag2 = 5;
+
+    Serial.println("Enter the initial month " );
+      while (Serial.available()==0){
+    
+    }
+    integer = Serial.parseInt();
+    digit_splitter(integer);
+    binary_converter(first_digits, last_digits);
+    bits_conc();
+    initial_bits();
+    flag2 = 6;
+
+    Serial.println( "Enter the initial year " );
+      while (Serial.available()==0){
+    
+    }
+    integer = Serial.parseInt();
+    digit_splitter(integer);
+    binary_converter(first_digits, last_digits);
+    bits_conc();
+    initial_bits();
+
+
+    //////////////////////////
+
+
+    
 }
 
-void loop()
-{
-  //int times = millis();
-  showTimeOnLCD();
+void loop(){
 
+  //Serial.println("you are in the loop ");
 }
 
-void showTimeOnLCD()
+
+
+
+
+int digit_splitter(int integers){
+    last_digits = integers % 10;
+    first_digits = (integers - last_digits) / 10;
+    // cout<<first_digits<<endl;
+    // cout<<last_digits<<endl;
+    return 0;
+}
+
+int day_set(String day)
 {
-  Wire.beginTransmission(deviceAddress); //START, Roll Cal
-  Wire.write(0x00); //pointing SEC Register at address 0x00 (Fig-2)
-  Wire.endTransmission(); //Execute the above queued data, ACK, STOP
 
-  Wire.requestFrom(deviceAddress, 7);   //SEC, MIN, and HRS to read from RTC as BCD
-  byte bcdSeconds = Wire.read();
-  byte bcdMinutes = Wire.read();
-  byte bcdHours = Wire.read();  //ignore Hours reading
-  byte bcdDay = Wire.read();
-  byte bcdDate = Wire.read();
-  byte bcdMonth = Wire.read();
-  byte bcdYear = Wire.read();
-  
-  //---show Time on LCD---------------
+    if (day == "Sunday")
+    {
+        bitval = "0b00000001";
+        bitvals = binaryStringToInt(bitval);
+    }
+    else if (day == "Monday")
+    {
+        bitval = "0b00000010";
+        bitvals = binaryStringToInt(bitval);
+    }
+    else if (day == "Tuesday")
+    {
+        bitval = "0b00000011";
+        bitvals = binaryStringToInt(bitval);
+    }
+    else if (day == "Wednesday")
+    {
+        bitval = "0b00000100";
+        bitvals = binaryStringToInt(bitval);
+    }
+    else if (day == "Thrusday")
+    {
+        bitval = "0b00000101";
+        bitvals = binaryStringToInt(bitval);
+    }
+    else if (day == "Friday")
+    {
+        bitval = "0b00000110";
+        bitvals = binaryStringToInt(bitval);
+    }
+    else if (day == "Saturday")
+    {
+        bitval = "0b00000111";
+        bitvals = binaryStringToInt(bitval);
+    }
+    else
+    {
+        bitval = "0b00000000";
+        bitvals = binaryStringToInt(bitval);
+    }
 
-  int day = bcdDay & 15;
-  String pot;
+    EEPROM[6] = bitvals;
 
-  switch(day){
- case 1: pot = "Sun";
- break;
- case 2:pot = "Mon";
- break;
- case 3:pot = "Tue";
- break;
- case 4:pot = "Wed";
- break;
- case 5:pot = "Thr";
- break;
- case 6:pot = "Fri";
- break;
- case 7:pot = "Sat";
- break;
- default: Serial.print("Invalid Input");
- break;
+    return 0;
+}
 
-  }
+int binary_converter(int first_digit, int last_digit)
+{
 
-  
-  lcd.print("Time: ");
-  lcd.print(bcdHours >> 4); lcd.print(bcdHours & 15); lcd.print(':');
-  lcd.print(bcdMinutes>> 4); lcd.print(bcdMinutes & 15); lcd.print(':');
-  lcd.print(bcdSeconds >> 4); lcd.print(bcdSeconds & 0x0F);
+    int j = 7;
+    int k = 3;
+    int result1 = 3;
+    int result2 = 3;
 
-  lcd.setCursor(0,1);// first argument which column second argument which row (up or down )
-  lcd.print(pot);
-  lcd.print(" : ");
-  lcd.print(bcdDate>>4);lcd.print(bcdDate & 15);lcd.print("/");
-  lcd.print(bcdMonth>>4);lcd.print(bcdMonth &15);lcd.print("/");
-  lcd.print(bcdYear>>4);lcd.print(bcdYear & 15);
-  lcd.setCursor(0,0);  //re=position the cursor
-  //delay(100);
+    while (divisor <= result1)
+    {
 
-//  Serial.print("Time: ");
-//  Serial.print(bcdHours);
-//  Serial.print(":");
-//  Serial.print(bcdMinutes);
-//  Serial.print(":");
-//  Serial.println(bcdSeconds);
-  
-//  Serial.print(bcdHours >> 4);Serial.print(bcdHours & 0x0F); Serial.print(':');
-//  //Serial.print(":");
-//  // Serial.println(bcdMinutes & 0x0F);
-//  Serial.println(bcdMinutes>> 4);// Serial.print(bcdMinutes & 0x0F); Serial.print(':');
-//  Serial.print(bcdSeconds >> 4); Serial.println(bcdSeconds & 0x0F);
- // lcd.setCursor(0, 0);  //re=position the cursor
+        result1 = last_digit / divisor;
+        bits[j] = last_digit % divisor;
+        last_digit = result1;
+        bits[j--];
+        if (result1 < divisor)
+        {
+            bits[j--] = result1;
+        }
+        else
+            continue;
+    }
+
+    while (divisor <= result2)
+    {
+
+        result2 = first_digit / divisor;
+        bits[k] = first_digit % divisor;
+        first_digit = result2;
+        bits[k--];
+        if (result2 < divisor)
+        {
+            bits[k--] = result2;
+        }
+        else
+            continue;
+
+        if (flag == 2)
+        {
+            Serial.println("Enter the time format. ");
+            Serial.println("For 12 hr time format enter 12, and for 24 hr time  format enter 24");
+            String period;
+            while(Serial.available() == 0){
+              
+            }
+            period = Serial.readString();
+            if (period == "12")
+            {bits[1] = 1; // sets the time format to 12 hr format
+             }
+            else bits[1]=0;// sets to 24 hr format 
+            if (bits[1] == 1)
+            {    
+                String cycle;
+                Serial.println(" Enter the clock  cycle that is am or pm");
+                Serial.println("For am clock cycle enter AM and for pm clock cycle enter PM");
+                while(Serial.available() ==0){
+                  
+                }
+                 cycle = Serial.readString();
+            if(cycle =="AM"){
+
+            }
+                bits[2] = 0; // setting it to am 
+            }
+            else bits[2] = 1;
+        }
+        else
+            continue;
+    }
+
+    return 0;
+}
+
+
+
+////////////////////////////////////////////////////////////////////
+void bits_conc(){ // concatenating the strings 
+    int v0, v1, v2, v3, v4, v5, v6, v7, v8;
+    String s0, s1, s2, s3, s4, s5, s6, s7;
+
+    for (int i = 0; i <= 8; i++)
+    {
+
+        if (i == 0)
+        {
+            v0 = bits[i];
+            s0 = String(v0);
+        }
+        else if (i == 1)
+        {
+            v1 = bits[i];
+            s1 = String(v1);
+        }
+        else if (i == 2)
+        {
+            v2 = bits[i];
+            s2 = String(v2);
+        }
+        else if (i == 3)
+        {
+            v3 = bits[i];
+            s3 = String(v3);
+        }
+        else if (i == 4)
+        {
+            v4 = bits[i];
+            s4 = String(v4);
+        }
+        else if (i == 5)
+        {
+            v5 = bits[i];
+            s5 = String(v5);
+        }
+        else if (i == 6)
+        {
+            v6 = bits[i];
+            s6 = String(v6);
+        }
+        else if (i == 7)
+        {
+            v7 = bits[i];
+            s7 = String(v7);
+        }
+    }
+    String cha = "0b";
+    if (flag2 == 0)
+    {
+        sec = cha + s0 + s1 + s2 + s3 + s4 + s5 + s6 + s7;
+        
+        Serial.println(sec);
+        secs = binaryStringToInt(sec);
+        Serial.println(secs);
+        EEPROM[3] = secs;
+    }
+    else if (flag2 == 1)
+    {
+        minss = cha + s0 + s1 + s2 + s3 + s4 + s5 + s6 + s7;
+        Serial.println(minss);
+        mins = binaryStringToInt(minss);
+        Serial.println(mins);
+        EEPROM[4] = mins;
+    }
+    else if (flag2 == 2)
+    {
+        hr = cha + s0 + s1 + s2 + s3 + s4 + s5 + s6 + s7;
+        Serial.println(hr);
+        hrs = binaryStringToInt(hr);
+        Serial.println(hrs);
+        EEPROM[5] = hrs;
+    }
+    else if (flag2 == 4)
+    {
+        date = cha + s0 + s1 + s2 + s3 + s4 + s5 + s6 + s7;
+        Serial.println(date);
+        dates = binaryStringToInt(date);
+        Serial.println(dates);
+        EEPROM[7] = dates;
+    }
+    else if (flag2 == 5)
+    {
+        month = cha + s0 + s1 + s2 + s3 + s4 + s5 + s6 + s7;
+        Serial.println(month);
+        months = binaryStringToInt(month);
+        Serial.println(months);
+        EEPROM[8] = months;
+    }
+    else if (flag2 == 6)
+    {
+        year = cha + s0 + s1 + s2 + s3 + s4 + s5 + s6 + s7;
+       Serial.println(year);
+        years = binaryStringToInt(year);
+        Serial.println(years);
+        EEPROM[9] = years;
+    }
+
+    return 0;
+}
+
+//////////////////////////////////////////////
+   void initial_bits(){
+    bits[0] = 0;
+    bits[1] = 0;
+    bits[2] = 0;
+    bits[3] = 0;
+    bits[4] = 0;
+    bits[5] = 0;
+    bits[6] = 0;
+    bits[7] = 0;
+    bits[8] = 0;
+   }
+
+
+
+unsigned int binaryStringToInt(String str)
+{
+    unsigned int mask = 1;
+    unsigned int result = 0;
+
+    for (int i = str.length() - 1; i >= 0; i--)
+    {
+        if (str[i] == '1')
+            result = result | mask;
+
+        if (str[i] == 'b')
+            break;
+
+        mask = mask << 1;
+    }
+
+    return result;
 }
