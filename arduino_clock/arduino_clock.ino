3913826052ffce73eb1c72239f6f67a9c8accc0f
Robin Sharma <cruzosharmas22545@gmail.com>
2020-11-24 21:59:51 +0530

    added features to set time using pushbuttons, but got some error in the setting menu

--- arduino_clock/arduino_clock.ino
+++ arduino_clock/arduino_clock.ino
@@ -6,7 +6,10 @@
 
 
 */
+
+// this code is in a total working condition as expected //// it is stored for future reference 
 #include<Wire.h>
+#include<EEPROM.h>
 #define deviceAddress 0b1101000 //0x68 the adddress of DS3231 clock module 
 #include<LiquidCrystal_I2C.h> // including the library for the display 
 LiquidCrystal_I2C lcd(0x27, 16, 2); // creating an display object (lcd), with address and size mentioned
@@ -75,7 +78,7 @@ byte Date ;
 byte Month ;
 byte Year ;
 
-int hrs, mins, secs; // variables to store the numeric time values from the clock 
+int hrs, mins, secs; // variables to store the numeric time values from the clock
 
 ////// setting up variable for the alaram system
 int curSec  ;
@@ -96,6 +99,30 @@ int cnt = 0;
 int button_state_old = 0;
 int button_state_new;
 
+int  btst_new1, btst_new2, state1, state2;
+int btst_old1 = 1;
+int btst_old2 = 1;
+//int btst_old3 = 1;
+
+int button1 = A2;
+int button2 = A1;
+int button3 = A0;
+
+int counter1 = 0;
+int counter2 = 0;
+int counter2_2 =0;
+int counter3 ;
+
+int variable = 0;
+
+int btst_new3;
+int btst_old3 = 1;
+int value ;
+//int  counter3 = 0;
+
+String app = "Time: 23:32:11";
+
+
 void setup() {
 
   Serial.begin(9600); // beginging the serial commmunication for debugging
@@ -111,6 +138,9 @@ void setup() {
   //Serial.print(val0);
 
   pinMode(btnPin, INPUT); // declaring pin 13 as input
+  pinMode(button1, INPUT);
+  pinMode(button2, INPUT_PULLUP);
+  pinMode(button3, INPUT_PULLUP);
   pinMode(12, OUTPUT);
 
 
@@ -118,14 +148,51 @@ void setup() {
 
 
 void loop() {
-
+  //
   button_state_new = digitalRead(btnPin);  // reading the state of  btnpin
   if (button_state_old == 1 && button_state_new == 0) {
+    lcd.clear();
     counter++; // incrementing the counter variable to flip the pages in the screen
-    lcd.clear(); // clearing the screen for removing the previous output
+    //if(counter !=6||counter!=7||counter!=5) counter1 = 1;
+    //lcd.clear(); // clearing the screen for removing the previous output
 
 
   }
+
+
+  ///////////////////////////// 1 ////////////////////
+
+  btst_new1 = digitalRead(button1);  // reading the state of  btnpin
+  if (btst_old1 == 1 && btst_new1 == 0) {
+    lcd.clear();
+    counter1++;
+  }
+
+  //////////////////////////////// 2 /////////////////////////
+
+  btst_new2 = digitalRead(button2);  // reading the state of  btnpin
+  if (btst_old2 == 1 && btst_new2 == 0) {
+    counter3 = 0;
+    if(counter == 5 && counter1 ==1)counter2_2++;
+    else if(counter ==5 && counter ==2) counter2++;
+    else counter2++;
+
+  }
+
+
+
+  btst_new3 = digitalRead(button3);  // reading the state of  btnpin
+  if (btst_old3 == 1 && btst_new3 == 0) {
+    counter3 ++;
+  }
+
+
+
+  //    if(counter2 ==5);counter2 = 1;
+  //  }
+
+
+
 
   ///////////////////////////////////////
 
@@ -135,7 +202,8 @@ void loop() {
   if (counter == 1) {
 
     ShowOnLcd(); // function that prints on the lcd screen
-    digitalWrite(12, HIGH); // for turning off the last digit in the segment display after a page flip
+    digitalWrite(12, HIGH); // for turning off the last digit in the segment display after a page fli
+
   }
 
   else if (counter == 2) {
@@ -148,6 +216,8 @@ void loop() {
     float to_min = diff * 60; // converting to minutes ;;
     int hrss = to_min / 60;
     int  minss = int(to_min) % 60;
+    //lcd.setCursor(0,0);
+    //lcd.print("Segment Time");
 
     value1 = hrss ; // passing the first value to be printed in the segment display
     value2 = minss; // passing the second value to be printed in the segment display
@@ -164,7 +234,7 @@ void loop() {
   else if (counter == 3) {
 
     ///// here we show the configured time for the alaram by the useer in the lcd display (when conter is 3 that is set to page 3 )///
-    
+
     digitalWrite(12, HIGH); // turing off the last digit in the segement display(bug);
     lcd.setCursor(1, 0); // setting the cursor position
     lcd.print("Alarm Set for :");
@@ -194,13 +264,181 @@ void loop() {
       lcd.print(ArMins);
     }
   }
+  else if (counter == 4) {
+    lcd.setCursor(0, 0);
+    lcd.print("Temp :");
+    lcd.print(32); lcd.print(" deg"); lcd.print(" C");
+    lcd.setCursor(5, 1);
+    lcd.print(":");
+    lcd.print(100); lcd.print(" deg"); lcd.print(" F");
+  }
+  else if (counter == 5 && counter1 ==0) {
+    lcd.setCursor(1, 0);
+    lcd.print("Setting Menu");
+   // lcd.setCursor(5, 1);
+    //lcd.print("Ahead");
+  }
+//  else if (counter == 6 && counter1 == 0) {
+//    lcd.setCursor(0, 0);
+//    lcd.print("Edit Date");
+//    lcd.setCursor(0, 1);
+//    lcd.print("and Time");
+//  }
+//  else if (counter == 7) {
+//    lcd.setCursor(0, 0);
+//    lcd.print("Edit Alarm");
+//  }
+
+//  else if (counter1 == 1 && counter ==5) {
+//    //lcd.clear();
+//    //counter = 0;
+//    lcd.setCursor(0, 0);
+//
+//    lcd.print("Time: ");
+//    ////lcd.print(counter3); lcd.print(" ");
+//    lcd.setCursor(8, 0);
+//    lcd.print(':');
+//    lcd.setCursor(11, 0);
+//    lcd.print(":");
+//    // // lcd.print(" "); lcd.print(" " ); lcd.print(':');
+//    ////lcd.print(" " ); lcd.print(0);
+//
+//    lcd.setCursor(0, 1); // first argument which column second argument which row (up or down )
+//    lcd.print("Day");
+//    lcd.print(" : ");
+//    ////lcd.print(0); lcd.print(0);
+//    lcd.setCursor(8, 1);
+//    lcd.print("/");
+//    // //lcd.print(0); lcd.print(0);
+//    lcd.setCursor(11, 1);
+//    lcd.print("/");
+//    lcd.print("  "); //lcd.print(0);
+//    lcd.setCursor(0, 0); //re=position the cursor
+//
+//    if (counter2 == 1) {
+//      ClockEdit(0, 1, 0x03, "Day");
+//      //lcd.setCursor(0,1)
+//    }
+//    else if (counter2 == 2) {
+//
+//      ClockEdit(6, 1, 0x04, "Date");
+//    }
+//    else if (counter2 == 3) {
+//      ClockEdit(9, 1, 0x05, "Month");
+//    }
+//    else if (counter2 == 4) {
+//      ClockEdit(12, 1, 0x06, "Year");
+//    }
+//    else if (counter2 == 5) {
+//      ClockEdit(6, 0, 0x02, "Hour");
+//    }
+//
+//    else if (counter2 == 6) {
+//      ClockEdit(9, 0, 0x01, "Minute");
+//    }
+//
+//    else if (counter2 == 7) {
+//      ClockEdit(12, 0, 0x00, "Second");
+//    }
+//
+//
+//
+//    if (counter2 == 8) counter2 = 1;
+//    //
+//  }
+
+    else if(counter1 == 1 && counter ==5){
+      //counter =0;
+       lcd.setCursor(1,0);lcd.print("SetTime");
+      lcd.setCursor(10,0);lcd.print("SetAlr");
+      lcd.setCursor(1,1);lcd.print("SetDate");
+      lcd.setCursor(10,1);lcd.print("SetDay");
+      switch(counter2_2){
+        case 1:lcd.setCursor(0,0);lcd.print(">");lcd.setCursor(9,1);lcd.print(" ");
+        break;
+        case 2:lcd.setCursor(0,1);lcd.print(">");lcd.setCursor(0,0);lcd.print(" ");
+        break;
+        case 3:lcd.setCursor(9,0);lcd.print(">");lcd.setCursor(0,1);lcd.print(" ");
+        break;
+        case 4:lcd.setCursor(9,1);lcd.print(">");lcd.setCursor(9,0);lcd.print(" ");
+        break;
+
+        
+  
+  
+      }
+      if (counter2_2 ==5)counter2_2 = 1;
+      }
+         if (counter2_2 ==1 && counter1 ==2){
+
+    lcd.setCursor(0, 0);
+
+    lcd.print("Time: ");
+    ////lcd.print(counter3); lcd.print(" ");
+    lcd.setCursor(8, 0);
+    lcd.print(':');
+    lcd.setCursor(11, 0);
+    lcd.print(":");
+
+    lcd.setCursor(0, 1); // first argument which column second argument which row (up or down )
+    lcd.print("Day");
+    lcd.print(" : ");
+    lcd.setCursor(8, 1);
+    lcd.print("/");
+    lcd.setCursor(11, 1);
+    lcd.print("/");
+    lcd.print("  "); //lcd.print(0);
+    lcd.setCursor(0, 0); //re=position the cursor
+
+    if (counter2 == 1) {
+      ClockEdit(0, 1, 0x03, "Day");
+    }
+    else if (counter2 == 2) {
+
+      ClockEdit(6, 1, 0x04, "Date");
+    }
+    else if (counter2 == 3) {
+      ClockEdit(9, 1, 0x05, "Month");
+    }
+    else if (counter2 == 4) {
+      ClockEdit(12, 1, 0x06, "Year");
+    }
+    else if (counter2 == 5) {
+      ClockEdit(6, 0, 0x02, "Hour");
+    }
+
+    else if (counter2 == 6) {
+      ClockEdit(9, 0, 0x01, "Minute");
+    }
+
+    else if (counter2 == 7) {
+      ClockEdit(12, 0, 0x00, "Second");
+    }
+
+
+
+//    if (counter2 == 8) counter2 = 1;
+     }
+//      else if (counter2 ==3 && counter1 ==2){
+//        lcd.setCursor(0,0);lcd.print("You did it");
+     // }
+
+
+
 
 
 
   button_state_old = button_state_new; // updating the variable
-  if (counter == 4) counter = 1; // setting the max num of pages in the screen to 3
+  btst_old1 = btst_new1;
+  btst_old2 = btst_new2;
+  // btst_old3 = btst_new3;
+  if (counter == 6) counter = 1; // setting the max num of pages in the screen to 3
+  if (counter1 == 3) counter1 = 0;
 
 }
+
+
+
 void display_seg_value1_f() {
 
   if (first_val == 0) {
@@ -508,17 +746,17 @@ void digit_splitter(int a) {
 
   second_val = a % 10;
   first_val  = (a - second_val) / 10;
-  Serial.println(second_val);
-  Serial.println(first_val);
+  //Serial.println(second_val);
+  //Serial.println(first_val);
 }
 
 void CollectData()
 {
-  Wire.beginTransmission(deviceAddress); // begining the transmission from the device 
-  Wire.write(0x00); // the first register out of the 7 registers 
-  Wire.endTransmission(); // end the transmission 
+  Wire.beginTransmission(deviceAddress); // begining the transmission from the device
+  Wire.write(0x00); // the first register out of the 7 registers
+  Wire.endTransmission(); // end the transmission
 
-  Wire.requestFrom(deviceAddress, 7); // requesting data from the 7 register of the clock module 
+  Wire.requestFrom(deviceAddress, 7); // requesting data from the 7 register of the clock module
   Seconds = Wire.read();
   Minutes = Wire.read();
   Hours = Wire.read();
@@ -528,9 +766,12 @@ void CollectData()
   Year = Wire.read();
 
   ////// calculating the numerical values of hrs mins and secs from the clock module /////
-  hrs = (Hours >> 4) * 10 + (Hours & 15); 
+  hrs = (Hours >> 4) * 10 + (Hours & 15);
   mins = (Minutes >> 4) * 10 + (Minutes & 15);
   secs = (Seconds >> 4) * 10 + (Seconds & 15);
+  //if(counter2 ==1)counter3 = hrs;
+  // else if(counter2 ==2) counter3 = mins;
+  //else if (counter2 ==3) counter3 = secs;
 }
 
 
@@ -581,4 +822,57 @@ float HrConverter (float hr, float min) {
 
 
   return hour;
+}
+
+void ClockEdit(int column, int row, int reg_address, String bucket) {
+
+  if (bucket == "Second") {
+    if (counter3 == 61)counter3 = 0;
+  }
+  if (bucket == "Minute") {
+    if (counter3 == 61)counter3 = 0;
+  }
+  if (bucket == "Hour") {
+    if (counter3 == 25)counter3 = 0;
+    lcd.setCursor(7, 0);
+    lcd.print(" ");
+  }
+  if (bucket == "Day") {
+    if (counter3 == 8)counter3 = 1;
+  }
+  if (bucket == "Date") {
+    if (counter3 == 32)counter3 = 1;
+  }
+  if (bucket == "Year") {
+    if (counter3 == 100)counter3 = 1;
+  }
+
+
+  lcd.setCursor(column, row); // for hours
+  lcd.blink();
+  delay(300);
+  value = counter3;
+
+  lcd.setCursor(column, row);
+  if (counter2 == 1) {
+
+    if (counter3 == 0)lcd.print("non");
+    else if (counter3 == 1)lcd.print("Sun");
+    else if (counter3 == 2)lcd.print("Mon");
+    else if (counter3 == 3)lcd.print("Tue");
+    else if (counter3 == 4)lcd.print("Wed");
+    else if (counter3 == 5)lcd.print("Thr");
+    else if (counter3 == 6)lcd.print("Fri");
+    else if (counter3 == 7)lcd.print("Sat");
+  }
+  else lcd.print(counter3);
+  //lcd.print(counter3);
+  //lcd.print("  ");
+  value = ((value / 10) << 4) + (value % 10);
+  Wire.beginTransmission(deviceAddress);
+  Wire.write(reg_address);                     // assessing the hour  register
+  Wire.write(value);               // wrinting to the hour register
+  Wire.endTransmission();
+  delay(300);
+
 }
