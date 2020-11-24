aa8426b56f2c90e5197773fe5c8a9b9733e8385f
Robin Sharma <cruzosharmas22545@gmail.com>
2020-11-24 21:38:08 +0530

    got good readings as expected

--- arduino_clock/arduino_clock.ino
+++ arduino_clock/arduino_clock.ino
@@ -1,64 +1,90 @@
 #include<Wire.h>
-#include<EEPROM.h>
-#define deviceAddress 0b1101000         //i2c address of the clock module  //  0x68 
+#define deviceAddress 0b1101000 //0x68
+#include<LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
 void setup()
 {
-  Wire.begin();                         // initalize the i2c module 
-
-
-////////////////// setting up the clock system /////////////////////
-
-// all the binaries are in bcd format 
-
- // seconds
- Wire.beginTransmission(deviceAddress); // begining transmission with the i2c module 
- Wire.write(0x00);                      // acccessing the seconds register 
- Wire.write(0b000000000);               // writing to the seconds register 
- Wire.endTransmission();                // ending transmission to the module
-
- //minutes
- Wire.beginTransmission(deviceAddress);
- Wire.write(0x01);                     // accessing the minutes  register 
- Wire.write(0b00000111);               //writing to the minutes register
- Wire.endTransmission();               //ending transmission to the module 
-
- //hour
- Wire.beginTransmission(deviceAddress);
- Wire.write(0x02);                     // assessing the hour  register 
- Wire.write(0b00010010);               // wrinting to the hour register 
- Wire.endTransmission();               //ending transmission to the module 
-
-  // day
-  Wire.beginTransmission(deviceAddress);
-  Wire.write(0x3);                     // accessing the day register
-  Wire.write(0b00000001);              // setting the day register to sunday 
-  Wire.endTransmission();              // ending transmission to the module 
-
-  // date
-  Wire.beginTransmission(deviceAddress);
-  Wire.write(0x4);                    // accessing the date register
-  Wire.write(0b00001000);             // setting the date register
-  Wire.endTransmission();
-
-  // month 
-  Wire.beginTransmission(deviceAddress);
-  Wire.write(0x5);                    // accessing the month  register
-  Wire.write(0b00010001);             // setting the month to register 
-  Wire.endTransmission();
-
-  // year 
-  Wire.beginTransmission(deviceAddress);
-  Wire.write(0x6);                    // accessing the year register
-  Wire.write(0b00100000);             // setting the year to register
-  Wire.endTransmission();
-
-
-
-  
+  Serial.begin(9600);
+  Wire.begin();   //I2C Bus is formed/eanbaled
+  lcd.begin();
+  lcd.backlight();
+  lcd.setCursor(0, 0); //DP0 of Topline
 }
 
 void loop()
 {
+  //int times = millis();
+  showTimeOnLCD();
 
-// do nothing 
+}
+
+void showTimeOnLCD()
+{
+  Wire.beginTransmission(deviceAddress); //START, Roll Cal
+  Wire.write(0x00); //pointing SEC Register at address 0x00 (Fig-2)
+  Wire.endTransmission(); //Execute the above queued data, ACK, STOP
+
+  Wire.requestFrom(deviceAddress, 7);   //SEC, MIN, and HRS to read from RTC as BCD
+  byte bcdSeconds = Wire.read();
+  byte bcdMinutes = Wire.read();
+  byte bcdHours = Wire.read();  //ignore Hours reading
+  byte bcdDay = Wire.read();
+  byte bcdDate = Wire.read();
+  byte bcdMonth = Wire.read();
+  byte bcdYear = Wire.read();
+  
+  //---show Time on LCD---------------
+
+  int day = bcdDay & 15;
+  String pot;
+
+  switch(day){
+ case 1: pot = "Sun";
+ break;
+ case 2:pot = "Mon";
+ break;
+ case 3:pot = "Tue";
+ break;
+ case 4:pot = "Wed";
+ break;
+ case 5:pot = "Thr";
+ break;
+ case 6:pot = "Fri";
+ break;
+ case 7:pot = "Sat";
+ break;
+ default: Serial.print("Invalid Input");
+ break;
+
+  }
+
+  
+  lcd.print("Time: ");
+  lcd.print(bcdHours >> 4); lcd.print(bcdHours & 15); lcd.print(':');
+  lcd.print(bcdMinutes>> 4); lcd.print(bcdMinutes & 15); lcd.print(':');
+  lcd.print(bcdSeconds >> 4); lcd.print(bcdSeconds & 0x0F);
+
+  lcd.setCursor(0,1);// first argument which column second argument which row (up or down )
+  lcd.print(pot);
+  lcd.print(" : ");
+  lcd.print(bcdDate>>4);lcd.print(bcdDate & 15);lcd.print("/");
+  lcd.print(bcdMonth>>4);lcd.print(bcdMonth &15);lcd.print("/");
+  lcd.print(bcdYear>>4);lcd.print(bcdYear & 15);
+  lcd.setCursor(0,0);  //re=position the cursor
+  //delay(100);
+
+//  Serial.print("Time: ");
+//  Serial.print(bcdHours);
+//  Serial.print(":");
+//  Serial.print(bcdMinutes);
+//  Serial.print(":");
+//  Serial.println(bcdSeconds);
+  
+//  Serial.print(bcdHours >> 4);Serial.print(bcdHours & 0x0F); Serial.print(':');
+//  //Serial.print(":");
+//  // Serial.println(bcdMinutes & 0x0F);
+//  Serial.println(bcdMinutes>> 4);// Serial.print(bcdMinutes & 0x0F); Serial.print(':');
+//  Serial.print(bcdSeconds >> 4); Serial.println(bcdSeconds & 0x0F);
+ // lcd.setCursor(0, 0);  //re=position the cursor
 }
